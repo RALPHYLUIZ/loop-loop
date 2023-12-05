e27deec6ae4074a82051c203066b1f98aa40ff53
ls1w <michelle_j.b@hotmail.com>
2023-12-05 09:28:05 -0300

    botão limpar ok

--- loop.js
+++ loop.js
@@ -20,3 +20,23 @@ function tabuadaWhile(){
     document.querySelector('#resultadoTabuada').innerHTML = linha;
 }
 
+function tabuadaFor(){
+
+    numero  = parseInt(document.querySelector('#slcNumero').value);
+
+    for (let contador = 0; contador <= 10; contador++) {
+
+        //console.log('VALOR DO CONTADOR: ' + contador);
+        resultado = numero + ' X ' + contador + ' = ' + (numero * contador);
+        linha += '<tr><td>' + resultado + '</td></tr>';
+        console.log(linha);
+    
+        
+    }
+    document.querySelector('#resultadoTabuada').innerHTML = linha;
+}
+
+function limpar() {
+
+    location.reload();
+}
